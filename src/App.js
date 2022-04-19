bd96c1b078cc92d7488877b743d7672da130b2c5
Eske Jayaswal <cph-ej162@cphbusiness.dk>
2022-04-19 14:08:15 +0200

    Første commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import './styling/App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Admin from './pages/Admin';
+import User from './pages/User';
+import Welcome from './pages/Welcome';
+import Data from './pages/Data';
+import Login from './pages/Login';
+import { useEffect, useState } from 'react'
+import facade from './apiFacade';
 
 function App() {
+
+  const init = { username: "", password: "" };
+  const [loginCredentials, setLoginCredentials] = useState(init);
+  const [loggedIn, setLoggedIn] = useState(false)
+
+
+  const onChange = (evt) => {
+      setLoginCredentials({ ...loginCredentials,[evt.target.id]: evt.target.value })
+  }
+
+  const performLogin = (evt) => {
+    evt.preventDefault();
+    login(loginCredentials.username, loginCredentials.password);
+  }
+
+  const logout = () => {  facade.logout()
+    setLoggedIn(false)
+  } 
+  const login = (user, pass) => { facade.login(user,pass)
+    .then(res =>setLoggedIn(true));
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Welcome/> }/>
+          <Route path="/user" element={<User/> }/>
+          <Route path="/admin" element={<Admin/> }/>
+          <Route path="/data" element={<Data/> }/>
+          <Route path="/login" element={<Login performLogin = { performLogin } onChange = { onChange } loggedIn = { loggedIn } performLogout = { logout } username = { loginCredentials.username }/> }/>
+        </Routes>
+      </Router>
     </div>
   );
 }
